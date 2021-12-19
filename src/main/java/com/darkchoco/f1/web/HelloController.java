75de12c259443ab7bbfc4eda61580395f5790f33
ssunghwan <ssunghwan1@gmail.com>
2021-12-19 21:14:19 +0900

    Test Code Study

---
+++ src/main/java/com/darkchoco/f1/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.darkchoco.f1.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
