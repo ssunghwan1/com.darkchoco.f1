ed4da8f43dd0e8fb40c8b27e450066397f745347
ssunghwan <ssunghwan1@gmail.com>
2021-12-20 21:31:15 +0900

    lombok study

---
+++ src/main/java/com/darkchoco/f1/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.darkchoco.f1.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
