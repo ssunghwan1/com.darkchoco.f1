2b5e3e0ccc49d552c5e381875f0db66460cf112a
CSH <ssunghwan1@gmail.com>
2021-12-22 21:28:37 +0900

    JPA START!!!

---
+++ src/main/java/com/darkchoco/f1/domain/posts/posts.java
@@ -0,0 +1,13 @@
+package com.darkchoco.f1.domain.posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+public class posts {
+
+    private Long id;
+
+}
