5e5ec26ac18a61f1549456c664264b8e5617421f
CSH <ssunghwan1@gmail.com>
2021-12-26 14:32:44 +0900

    JPA crated Date, modified Date

--- src/main/java/com/darkchoco/f1/domain/posts/Posts.java
+++ src/main/java/com/darkchoco/f1/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.darkchoco.f1.domain.posts;
 
+import com.darkchoco.f1.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/darkchoco/f1/springboot/domain/posts/PostRepositoryTest.java
+++ src/test/java/com/darkchoco/f1/springboot/domain/posts/PostRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -44,6 +46,32 @@ public class PostRepositoryTest {
         //then
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2021,6,4,0,0,0);
+        postsRepositoory.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepositoory.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createdDate " + posts.getCreatedDate()+", modifiedDate=" + posts.getModifiedDate()) ;
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
+
+
+
     }
 
 }
