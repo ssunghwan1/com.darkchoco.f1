5ad4c409ae961c857ca835133a2bc8cdbddc0b25
CSH <ssunghwan1@gmail.com>
2021-12-27 21:42:08 +0900

    mustache Start!!

--- src/main/java/com/darkchoco/f1/service/posts/PostsService.java
+++ src/main/java/com/darkchoco/f1/service/posts/PostsService.java
@@ -2,13 +2,18 @@ package com.darkchoco.f1.service.posts;
 
 import com.darkchoco.f1.domain.posts.Posts;
 import com.darkchoco.f1.domain.posts.PostsRepository;
+import com.darkchoco.f1.web.dto.PostsListResponseDto;
 import com.darkchoco.f1.web.dto.PostsResponseDto;
 import com.darkchoco.f1.web.dto.PostsSaveRequestDto;
 import com.darkchoco.f1.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +40,19 @@ public class PostsService {
     }
 
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
+    }
 
 }
--- src/main/java/com/darkchoco/f1/web/PostsApiController.java
+++ src/main/java/com/darkchoco/f1/web/PostsApiController.java
@@ -27,4 +27,10 @@ public class PostsApiController {
     public PostsResponseDto findbyId (@PathVariable Long id){
         return postsService.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete (@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
 }
