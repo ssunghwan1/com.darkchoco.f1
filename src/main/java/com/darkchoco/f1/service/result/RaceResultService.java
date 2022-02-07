ee1f3d1b0cbddd53f21fe7fa29b9f6db4b5920b8
CSH <ssunghwan1@gmail.com>
2022-02-07 17:08:52 +0900

    테이블명 소문자로 변경

--- src/main/java/com/darkchoco/f1/service/result/RaceResultService.java
+++ src/main/java/com/darkchoco/f1/service/result/RaceResultService.java
@@ -57,7 +57,7 @@ public class RaceResultService {
 
     public List<Map<String, Object>> findRankByTitle (String title){
         List<Map<String, Object>> result = jdbcTemplate.queryForList("SELECT rank () over (order by sum(point) desc) as rank," +
-                "sum(point) as point , driver from  Race_Result    where title = ? group by driver order by rank ",title);
+                "sum(point) as point , driver from  race_result    where title = ? group by driver order by rank ",title);
         return result;
     }
 
